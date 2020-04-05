c5fcd52a0ce24c615c0d51c5b69caaf27bb2d3f4
Mert  Ozkaynak <mertozkaynak@windowslive.com>
2020-04-05 23:46:22 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,8 +4,16 @@ def game_hash
     :home =>{ 
       :team_name => "Brooklyn Nets",
       :colors => ["Black", "White"],
-      :players => [
-        {
+      :players => [{
+        :player_name =>
+        :number
+        :shoe
+        :points
+        :rebounds
+        :assists
+        :steals
+        :blocks
+        :slam_dunks}
         
         
         
