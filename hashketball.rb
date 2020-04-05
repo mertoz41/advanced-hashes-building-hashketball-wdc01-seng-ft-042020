a2b1145ae1b14a497d9354cba45e4dac1587fe89
Mert  Ozkaynak <mertozkaynak@windowslive.com>
2020-04-05 23:59:49 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -33,7 +33,7 @@ def game_hash
           :assists => 10,
           :steals => 3,
           :blocks => 1,
-          :slam_dunks = 15},
+          :slam_dunks => 15},
           {
             :player_name => "Mason Plumlee",
           :number => 1, 
